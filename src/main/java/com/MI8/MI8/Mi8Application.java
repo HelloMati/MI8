c74faa85f01f312d1cdff89472ff6aef44eb49fa
J-emery <jamzwj@gmail,com>
2023-02-20 11:19:06 +0000

    added Spring framework

---
+++ src/main/java/com/MI8/MI8/Mi8Application.java
@@ -0,0 +1,13 @@
+package com.MI8.MI8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mi8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mi8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/MI8/MI8/Mi8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MI8.MI8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mi8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
