2dcb5d73a4dbaff78992bf43b8c92c1730cd83ee
J-emery <jamzwj@gmail,com>
2023-02-20 16:01:16 +0000

    fixed issues

--- src/main/java/com/MI8/MI8/repositories/PlayerCharacterRepositories.java
+++
@@ -1,7 +0,0 @@
-package com.MI8.MI8.repositories;
-
-import com.MI8.MI8.models.PlayerCharacter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerCharacterRepository extends JpaRepository<PlayerCharacter,Integer> {
-}
