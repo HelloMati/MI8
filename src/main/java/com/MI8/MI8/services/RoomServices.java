42b099f8391afd447f090f2f3af895a5f974e011
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-02-20 16:33:10 +0000

    created Room Packages

---
+++ src/main/java/com/MI8/MI8/services/RoomServices.java
@@ -0,0 +1,16 @@
+package com.MI8.MI8.services;
+
+import com.MI8.MI8.models.Room;
+import com.MI8.MI8.repositories.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomServices {
+
+@Autowired
+    RoomRepository roomRepository;
+
+    public Room CheckCurrentRoom
+
+}
