0ae990d77646ed86c47a11ba23c1bc7d16001c21
HelloMati <118129617+HelloMati@users.noreply.github.com>
2023-02-21 10:58:03 +0000

    updates for PlayerServices, PlayerController

--- src/main/java/com/MI8/MI8/controllers/PlayerController.java
+++ src/main/java/com/MI8/MI8/controllers/PlayerController.java
@@ -1,6 +1,5 @@
 package com.MI8.MI8.controllers;
 
-
 import com.MI8.MI8.models.Player;
 import com.MI8.MI8.services.PlayerServices;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,31 +15,25 @@ public class PlayerController {
     PlayerServices playerService;
 
     @PostMapping
-    public ResponseEntity<Player> CreateNewPlayer(@RequestParam String name) {
+    public ResponseEntity<Player> createNewPlayer(@RequestParam String name) {
         Player player = playerService.createPlayerCharacter(name);
-        return new ResponseEntity(player, HttpStatus.CREATED);
+        return new ResponseEntity<>(player, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Player> getCharacter(@PathVariable int id){
-        return new  ResponseEntity<>(playerService.getCharacter(id), HttpStatus.OK);
+    public ResponseEntity<Player> getCharacter(@PathVariable int id) {
+        return new ResponseEntity<>(playerService.getCharacter(id), HttpStatus.OK);
     }
 
-//    @GetMapping(value = "/{id}")
-//    public ResponseEntity<Player> getPlayerId(@PathVariable int id){
-//        Player player = playerService.getPlayerId(id);
-//        return new ResponseEntity<>(player, HttpStatus.OK);
-//    }
-//
-//    @PatchMapping(value = "/{id}")
-//    public ResponseEntity<Player> updatePlayer(@PathVariable int id, @RequestParam Player player){
-//        Player updatedPlayer = playerService.updatePlayer(id, player);
-//        return new ResponseEntity<>(updatedPlayer, HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping(value = "/{id}")
-//    public ResponseEntity deletePlayer(@PathVariable int id){
-//        playerService.deletePlayer(id);
-//        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-//    }
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Player> updatePlayer(@PathVariable int id, @RequestBody Player player) {
+        Player updatedPlayer = playerService.updatePlayer(id, player);
+        return new ResponseEntity<>(updatedPlayer, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deletePlayer(@PathVariable int id) {
+        playerService.deletePlayer(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
